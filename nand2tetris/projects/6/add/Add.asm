7119ecd4c025afae83640ad47d889b588bb1fa3e
ABDURRAHMAN <romaissaeabde@gmail.com>
2025-10-02 02:06:44 +0100

    feat: complete Nand2Tetris Project 6 - Assembler

--- nand2tetris/projects/6/add/Add.asm
+++ nand2tetris/projects/6/add/Add.asm
@@ -1,24 +1,14 @@
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
-						// by Nisan and Schocken, MIT Press.
-  // File name: projects/6/add/Add.asm
+// by Nisan and Schocken, MIT Press.
+// File name: projects/06/add/Add.asm
 
-// Computes R0 = 2 + 3  (R0 refers to RAM[0])
+// Computes R0 = 2 + 3
 
-(RANDOM_LABEL)
-//@2
+@2
+D=A
+@3
 D=D+A
-D=A      // hahahah wesh
-//@3
-D=D+A
-//@0
-M=D;jmp
-//tests
-//@sum
+@0
 M=D
-//@R0
-M=d
-//@KBD
-M+1
-//@RANDOM_LABEL
-M=D
+
