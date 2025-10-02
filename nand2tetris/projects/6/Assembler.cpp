a0b873e529bbfa2c6a81595684de2eb1fd6320a5
ABDURRAHMAN <romaissaeabde@gmail.com>
2025-10-02 01:05:24 +0100

    Switching from Mac os to Arch Linux

--- nand2tetris/projects/6/Assembler.cpp
+++ nand2tetris/projects/6/Assembler.cpp
@@ -4,28 +4,128 @@
 
 void Assembler::assemble(string inFile, string outFile) {
 	
-
+	initSymbolTable();
 	ifstream inputFile(inFile);
-	if (inputFile.is_open()) {
-		string line;
-		int romAddress = 0;
+	ofstream outputFile(outFile);
 
-		while (getline(inputFile, line)) {
-			string cleanedLine = cleanLine(line);
-			
-			if (cleanedLine == "") continue;
-			else if (Assembler::isLabel(cleanedLine)) {
-				string label = Assembler::extractLabel(cleanedLine);
+	if (inputFile.is_open()) {
+		string line; int romAddress = 0;
+
+		PassOne(inputFile, line, romAddress);
+		inputFile.clear(); inputFile.seekg(0);
+		PassTwo(inputFile, outputFile, line);
+
+		Assembler::printMap();
+		/* Test scop */
+		aInstructionToBinary("@16");
+		/* End */
+		inputFile.close();
+	} 
+	else cerr << "Error in opening file." << endl;
+}
+
+void Assembler::PassTwo(ifstream& inputFile, ofstream& outputFile, string& line) {
+	while (getline(inputFile, line)) {
+		string cleanedLine = cleanLine(line);
+		if (cleanedLine == "" || isLabel(cleanedLine)) continue;
+
+		if (cleanedLine[0] == '@') {
+			string binary = aInstructionToBinary(cleanedLine.erase(0, 1));
+			outputFile << binary + '\n';
+		} else {
+			string binary = cInstructionToBinary(cleanedLine);
+			outputFile << binary + '\n';
+		}
+	}	
+}
+
+void Assembler::PassOne(ifstream& inputFile, string& line, int& romAddress)  {
+	while (getline(inputFile, line)) {
+		string cleanedLine = cleanLine(line);
+		
+		if (cleanedLine == "") continue;
+		else if (Assembler::isLabel(cleanedLine)) {
+			string label = Assembler::extractLabel(cleanedLine);
 			cout << "label: " << label << endl;
 			Assembler::symbolTable[label] = romAddress;
 		}
 		else romAddress++;
 		cout << "'" << cleanedLine << "'" << ",   " << "romAddress: " << romAddress << endl;
-		}
-		Assembler::printMap();
-		inputFile.close();
-	} 
-	else cerr << "Error in opening file." << endl;
+	}
+}
+
+vector<string> Assembler::split(const string& s, const string& delimiters) {
+	vector<string> tokens;
+	size_t lastPos = 0;
+	size_t pos = s.find_first_of(delimiters, lastPos);
+
+	while (pos != string::npos) {
+		tokens.push_back(s.substr(lastPos, (pos - lastPos)));
+		lastPos = pos+1;
+		pos = s.find_first_of(delimiters, lastPos);
+	}
+	tokens.push_back(s.substr(lastPos)); // latest we7da
+	return tokens;
+}
+
+/* 111accccccdddjjj
+ *
+ * 1: dest=comp 
+ * 2: comp;jmp 
+ * 3: dest=comp;jmp 
+ * 4: comp 
+ *
+ * */
+string Assembler::cInstructionToBinary(string s) {
+	vector<string> tokens = split(s, "=;");
+	string binary;
+
+	for (auto &tok : tokens)
+    	std::transform(tok.begin(), tok.end(), tok.begin(), ::toupper);
+
+
+	if (tokens.size() == 1) {
+			binary = compMap[tokens[0]] + destMap[""] + jumpMap[""];
+			return "111" + binary;
+	} else if (tokens.size() == 2) {
+		if (tokens[1][0] == 'J') binary = compMap[tokens[0]] + destMap[""] + jumpMap[tokens[1]];
+		else binary = compMap[tokens[0]] + destMap[tokens[1]] + jumpMap[""];
+		cout << "binary case check: " << binary << endl;
+		return "111" + binary;
+	} else {
+		return "111" + compMap[tokens[0]] + destMap[tokens[1]] + jumpMap[tokens[2]];
+	}
+	return "";
+}
+
+string Assembler::aInstructionToBinary(string value) {
+	char *p;
+	const char *s = value.c_str();
+	long converted = strtol(s, &p, 10);
+
+	if (*p) {
+		if (symbolTable.find(value) == symbolTable.end()) symbolTable[value] = nextRamAddress++;
+		
+		cout << "NextRamAddress: " << symbolTable[value] << endl;
+		string binary = bitset<15>(symbolTable[value]).to_string();
+		return "0" + binary;
+	}
+	string binary = bitset<15>(converted).to_string();
+	return "0" + binary;
+}
+
+void Assembler::initSymbolTable() {
+	/* R0 -> R15, and other predefined ones */
+	for (int i = 0; i <= 15; i++) 
+		Assembler::symbolTable["R" + to_string(i)] = i;
+
+	Assembler::symbolTable["SP"] 	 = 0;
+	Assembler::symbolTable["LCL"] 	 = 1;
+	Assembler::symbolTable["ARG"] 	 = 2;
+	Assembler::symbolTable["THIS"] 	 = 3;
+	Assembler::symbolTable["THAT"] 	 = 4;
+	Assembler::symbolTable["SCREEN"] = 16384;
+	Assembler::symbolTable["KBD"] 	 = 24576; 
 }
 
 string Assembler::extractLabel(string line) {
@@ -61,10 +161,10 @@ void Assembler::printMap() {
 
 int main (int ac, char* av[]) {
 	Assembler assembler;
-	try {
-		assembler.assemble(av[1], av[2]);
-	} catch (const logic_error& e) {
-		cerr << "An unknown error occurred --> " << e.what()  << endl;
-	}
 
+	string inFile= av[1], temp = av[1];
+	string outFile = temp.erase(temp.size() - 4) + ".hack";
+
+	try { assembler.assemble(inFile, outFile);
+	} catch (const exception& e) { cerr << "An unknown error occurred --> " << e.what()  << endl; }
 }
--- nand2tetris/projects/6/add/Add.asm
+++ nand2tetris/projects/6/add/Add.asm
@@ -6,9 +6,18 @@
 // Computes R0 = 2 + 3  (R0 refers to RAM[0])
 
 (RANDOM_LABEL)
-@2
+//@2
 D=A      // hahahah wesh
-@3
+//@3
 D=D+A
-@0
+//@0
+M=D;jmp
+//tests
+//@sum
+M=D
+//@R0
+M=d
+//@KBD
+M+1
+//@RANDOM_LABEL
 M=D
